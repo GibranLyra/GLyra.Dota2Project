4fc6690f1febf7aade5e7a0995ea8255314d9b96
Gibran Lyra <lyra.gibran@gmail.com>
2014-02-07 15:30:52 -0200

    no message

---
+++ GLyra.Dota2/GLyra.Dota2/BaseRepositoryApi.cs
@@ -0,0 +1,81 @@
+﻿using System.Net;
+using Newtonsoft.Json;
+using System;
+using PortableSteam;
+using System.Configuration;
+
+namespace Dota.CentralDota
+{
+    public class BaseRepositoryApi
+    {
+        #region Properties
+
+        public enum APIVersion { v001, v0001, v0002 };
+
+        public string SteamId { get; set; }
+        public string SteamInterface { get; set; }
+
+        private string _urlApiSteam;
+        string UrlApiSteam
+        {
+            get { return _urlApiSteam; }
+        }
+
+        private string _steamApiKey;
+        string SteamApiKey
+        {
+            get { return _steamApiKey; }
+        }
+        #endregion
+
+        public BaseRepositoryApi(string steamId)
+        {
+            this.SteamId = steamId;
+            _urlApiSteam = ConfigurationManager.AppSettings.Get("UrlApiSteam");
+            _steamApiKey = ConfigurationManager.AppSettings.Get("SteamApiKey");
+            bool isTesting = bool.Parse(ConfigurationManager.AppSettings.Get("Testing"));
+
+            //In case of test use Beta TEST API(_205790)
+            if (isTesting)
+                SteamInterface = "_205790";
+            else
+                SteamInterface = "_570";            
+        }
+
+        public T GetAPIResult<T>(string steamInterface, string method, APIVersion apiVersion, string steamId) where T : new()
+        {
+            using (var w = new WebClient())
+            {
+                var jsonData = string.Empty;
+                string url = string.Format("{0}/{1}/{2}/{3}/{4}", UrlApiSteam, steamInterface, method, apiVersion, "?key=" + SteamApiKey, SteamId);
+
+                try
+                {
+                    jsonData = w.DownloadString(url);
+                    
+                }
+                catch (Exception e)
+                {
+                    throw e;
+                }
+                // if string with JSON data is not empty, deserialize it to class and return its instance 
+                return !string.IsNullOrEmpty(jsonData) ? JsonConvert.DeserializeObject<T>(jsonData) : new T();
+            }
+        }
+
+        //Sample Url: http://api.steampowered.com/IDOTA2Match_570/GetMatchHistory/v1//?key=7CC0589DA258E2792540EDCAF588D63E
+        public void getMatchHistory()
+        {
+            SteamInterface = "IDOTA2Match" + SteamInterface;
+            GetAPIResult<object>(SteamInterface, "GetMatchHistory", APIVersion.v0001, this.SteamId);
+        }
+
+        //Sample Url: https://api.steampowered.com/IEconDOTA2_570/GetHeroes/v0001/
+        public void getHeroes()
+        {
+            SteamInterface = "IEconDota2" + SteamInterface;
+            //TODO create a localization rule: https://api.steampowered.com/IEconDOTA2_570/GetHeroes/v0001/?key=<key>&language=en_us
+            GetAPIResult<object>(SteamInterface, "GetHeroes", APIVersion.v0001, this.SteamId);
+        }
+    }
+}
---
+++ LogHandler.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dota.CentralDota
+{
+    public static class LogHandler
+    {
+        /// <summary>
+        /// Create a log in case of a item that's supposed to have only one value has more than one
+        /// </summary>
+        /// <param name="nodeName"></param>
+        public static void createWarningLog(string nodeName, int expectedSize, int actualSize)
+        {
+            Debug.WriteLine("Warning, node " + nodeName + " supposed to have: " + expectedSize  + " item(s), but have " + actualSize);
+        }
+    }
+}
